50c861682b42ad00bef645aa1bff12669308f51a
ialexjx <akshathrx6393@gmail.com>
2024-08-09 22:11:21 +0530

    Print string

---
+++ src/main/java/basics/string/PrintString.java
@@ -0,0 +1,7 @@
+package basics.string;
+
+public class PrintString {
+    public static void main(String[] args) {
+        System.out.println("Hello there, This is a java program to print the string");
+    }
+}
